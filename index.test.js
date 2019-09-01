1ddfcff2294da95d1f80af86d78aedafe98ade54
Christian Fei <crifei93@gmail.com>
2019-09-01 10:00:32 +0200

    test product details and search

--- index.test.js
+++ index.test.js
@@ -1,5 +1,25 @@
 const tap = require('tap')
-tap.test('works', t => {
+const got = require('got')
+
+tap.test('get page html content', async t => {
   t.plan(1)
-  t.pass(42)
+
+  const html = await getHtml('porta carta credito')
+
+  t.true(html.body.indexOf('s-search-results'))
 })
+
+tap.test('get product details', async t => {
+  t.plan(1)
+
+  const html = await getProductDetails('B07VF7YVX4')
+
+  t.true(html.body.indexOf('B07VF7YVX4'))
+})
+
+async function getHtml (search) {
+  return got(`https://www.amazon.it/s?k=${encodeURIComponent(search)}`)
+}
+async function getProductDetails (asin) {
+  return got(`https://www.amazon.it/dp/${asin}`)
+}
