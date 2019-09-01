7d57ef47ef7dec65985e05fb16fe0ef5c16cd9ca
Christian Fei <crifei93@gmail.com>
2019-09-01 10:44:43 +0200

    cleanup

--- index.test.js
+++ index.test.js
@@ -3,51 +3,41 @@ const got = require('got')
 const $ = require('cheerio')
 
 tap.test('get page html content', async t => {
-  t.plan(1)
-
   const html = await fetchSearchHtml('porta carta credito')
-
+  t.plan(1)
   t.true(html.indexOf('s-search-results'))
 })
 
 tap.test('get product details', async t => {
-  t.plan(1)
-
   const html = await fetchProductDetailsHtml('B07VF7YVX4')
-
+  t.plan(1)
   t.true(html.indexOf('B07VF7YVX4'))
 })
 
 tap.test('get product reviews', async t => {
-  t.plan(1)
-
   const html = await fetchProductReviewsHtml('B07VF7YVX4')
-
+  t.plan(1)
   t.true(html.indexOf('B07VF7YVX4'))
 })
 
 tap.test('get product reviews paginated', async t => {
-  t.plan(1)
-
   const html = await fetchProductReviewsHtml('B07VF7YVX4', 2)
-
+  t.plan(1)
   t.true(html.indexOf('B07VF7YVX4'))
 })
 
 tap.test('parses product reviews paginated', async t => {
-  t.plan(1)
-
   const html = await fetchProductReviewsHtml('B07VF7YVX4')
   const reviews = parseProductReviews(html)
+  t.plan(1)
   t.true(reviews.length > 0)
 })
 
 tap.test('extracts review from html', async t => {
-  t.plan(4)
-
   const html = await fetchProductReviewsHtml('B07VF7YVX4')
   const reviews = parseProductReviews(html)
   const review = extractReviewFromHtml(reviews[0])
+  t.plan(4)
   t.true(review)
   t.true(review.stars > 0 && review.stars <= 5)
   t.true(review.dateString)
@@ -74,11 +64,11 @@ function extractReviewFromHtml (html) {
   const dateString = $('[data-hook="review-date"]', html).text()
   const text = $('[data-hook="review-body"]', html).text()
   let stars
-  if (starsContent.hasClass('a-star-5')) stars = 5
-  if (starsContent.hasClass('a-star-4')) stars = 4
-  if (starsContent.hasClass('a-star-3')) stars = 3
-  if (starsContent.hasClass('a-star-2')) stars = 2
-  if (starsContent.hasClass('a-star-1')) stars = 1
+  if (!stars && starsContent.hasClass('a-star-5')) stars = 5
+  if (!stars && starsContent.hasClass('a-star-4')) stars = 4
+  if (!stars && starsContent.hasClass('a-star-3')) stars = 3
+  if (!stars && starsContent.hasClass('a-star-2')) stars = 2
+  if (!stars && starsContent.hasClass('a-star-1')) stars = 1
 
   return {
     stars,
