3b1a39bbd6cd060fb1b8b77c91378a84d5f776e1
Christian Fei <crifei93@gmail.com>
2019-11-01 18:46:20 +0100

    perf

--- bin.js
+++ bin.js
@@ -2,7 +2,9 @@
 
 const amazon = require('./amazon')
 const { default: PQueue } = require('p-queue')
-const queue = new PQueue({ concurrency: 50, timeout: 30000 })
+const pLimit = require('p-limit')
+const limit = pLimit(6)
+const queue = new PQueue({ concurrency: 6, timeout: 30000 })
 const log = require('debug')('bin')
 
 main(process.argv[2], process.argv[3])
@@ -29,31 +31,49 @@ async function main (asin, pageNumber = 1) {
 
   const pageSize = firstPageReviews.length
   const pages = parseInt(productReviewsCount / pageSize, 10) + 1
-  let lastPageSize = pageSize
 
-  const allReviews = []
-  for (; pageNumber < pages && lastPageSize > 0; pageNumber++) {
-    log(`Processing ${pageSize} / ${pages}`)
+  const tasks = Array.from({ length: pages }, (_, i) => {
+    return i + pageNumber
+  })
+
+  let noMoreReviewsPageNumber
+  let allReviewsCount = 0
+
+  await Promise.all(tasks.map((pageNumber) => limit(async () => {
+    if (noMoreReviewsPageNumber) {
+      log(`Skipping ${pageNumber} / ${pages} (noMoreReviewsPageNumber ${noMoreReviewsPageNumber})`)
+      return []
+    }
+    log(`Processing ${pageNumber} / ${pages}`)
     const asinPageNumberExists = require('fs').existsSync(require('path').resolve(__dirname, 'json', `${asin}-${pageNumber}.json`))
 
     let productReviews = []
     if (asinPageNumberExists && !process.env.NO_CACHE) {
-      log(`Using json/${asin}-${pageNumber}.json`)
-      const content = require('fs').readFileSync(require('path').resolve(__dirname, 'json', `${asin}-${pageNumber}.json`), { encoding: 'utf8' })
-      productReviews = JSON.parse(content)
+      productReviews = await queue.add(() => {
+        log(`Using json/${asin}-${pageNumber}.json`)
+        const content = require('fs').readFileSync(require('path').resolve(__dirname, 'json', `${asin}-${pageNumber}.json`), { encoding: 'utf8' })
+        return JSON.parse(content)
+      })
     } else {
-      log(`Scraping page ${pageNumber} for asin ${asin}`)
-      productReviews = await queue.add(() => amazon.getProductReviews({ asin, pageNumber }, { useProxy: true }))
+      productReviews = await queue.add(() => {
+        log(`Scraping page ${pageNumber} for asin ${asin}`)
+        return amazon.getProductReviews({ asin, pageNumber }, { useProxy: true })
+      })
     }
 
-    allReviews.push(...productReviews)
+    allReviewsCount += productReviews.length
+    if (productReviews.length === 0 && !noMoreReviewsPageNumber) {
+      noMoreReviewsPageNumber = pageNumber
+    }
 
-    log(`Found ${productReviews && productReviews.length} product reviews on page ${pageNumber} for asin ${asin}`)
-    log(`Accuracy ${((allReviews.length / productReviewsCount) * 100).toFixed(1)}% (${allReviews.length} / ${productReviewsCount})`)
+    log(`Found ${productReviews && productReviews.length} product reviews on page ${pageNumber} / ${pages} for asin ${asin}`)
 
-    pageNumber++
-    lastPageSize = productReviews.length
-  }
+    // pageNumber++
+    // lastPageSize = productReviews.length
+    log(`Accuracy ${((allReviewsCount / productReviewsCount) * 100).toFixed(1)}% (${allReviewsCount} / ${productReviewsCount})`)
+    return productReviews
+  })))
+    .then((...results) => results.reduce((acc, curr) => acc.concat(curr), []))
 
   await queue.onIdle()
   log('All work is done')
