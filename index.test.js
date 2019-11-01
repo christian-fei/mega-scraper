96c2c5a7487fa775eb899efd67559aa021b5399a
Christian Fei <crifei93@gmail.com>
2019-11-01 13:11:30 +0100

    more detailed assertions

--- index.test.js
+++ index.test.js
@@ -11,33 +11,38 @@ const {
 
 test('get page html content', async t => {
   const html = await fetchSearchHtml('porta carta credito')
-  t.plan(1)
+  t.plan(2)
+  t.true(typeof html === 'string')
   t.true(html.indexOf('s-search-results'))
 })
 
 test('get product details', async t => {
   const html = await fetchProductDetailsHtml('B07JML91PY')
-  t.plan(1)
+  t.plan(2)
+  t.true(typeof html === 'string')
   t.true(html.indexOf('B07JML91PY'))
 })
 
 test('get product reviews', async t => {
   const html = await fetchProductReviewsHtml('B07JML91PY')
-  t.plan(1)
+  t.plan(2)
+  t.true(typeof html === 'string')
   t.true(html.indexOf('B07JML91PY'))
 })
 
 test('get product reviews paginated', async t => {
   const html = await fetchProductReviewsHtml('B07JML91PY', 2)
-  t.plan(1)
+  t.plan(2)
+  t.true(typeof html === 'string')
   t.true(html.indexOf('B07JML91PY'))
 })
 
 test('parses product reviews paginated', async t => {
   const html = await fetchProductReviewsHtml('B07JML91PY')
   const reviews = parseProductReviews(html)
-  t.plan(1)
-  t.true(reviews)
+  t.plan(2)
+  t.true(Array.isArray(reviews))
+  t.true(reviews.length > 0)
 })
 
 test('extracts review from html', async t => {
