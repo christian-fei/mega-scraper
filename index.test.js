acb8cbc58ffee6202e5e5b7b7da242543e906f32
Christian Fei <crifei93@gmail.com>
2019-11-01 13:10:00 +0100

    more detailed assertions

--- index.test.js
+++ index.test.js
@@ -42,9 +42,11 @@ test('parses product reviews paginated', async t => {
 
 test('extracts review from html', async t => {
   const html = await fetchProductReviewsHtml('B07JML91PY')
+  t.plan(6)
   const reviews = parseProductReviews(html)
+  t.true(Array.isArray(reviews))
+  t.true(reviews.length > 0)
   const review = extractReviewFromHtml(reviews[0])
-  t.plan(4)
   t.true(review)
   t.true(review.stars > 0 && review.stars <= 5)
   t.true(review.dateString)
@@ -53,8 +55,10 @@ test('extracts review from html', async t => {
 
 test('gets reviews from product search', async t => {
   const reviews = await getProductReviews('B07JML91PY')
+  t.plan(6)
+  t.true(Array.isArray(reviews))
+  t.true(reviews.length > 0)
   const review = reviews[0]
-  t.plan(4)
   t.true(review)
   t.true(review.stars > 0 && review.stars <= 5)
   t.true(review.dateString)
@@ -63,8 +67,10 @@ test('gets reviews from product search', async t => {
 
 test('gets reviews from product search with proxy', async t => {
   const reviews = await getProductReviews('B07JML91PY', 1, { useProxy: true })
+  t.plan(6)
+  t.true(Array.isArray(reviews))
+  t.true(reviews.length > 0)
   const review = reviews[0]
-  t.plan(4)
   t.true(review)
   t.true(review.stars > 0 && review.stars <= 5)
   t.true(review.dateString)
