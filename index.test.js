0dc6b4e2c4b7d2c028250f20916fb5ed287acc90
Christian Fei <crifei93@gmail.com>
2019-11-01 13:00:52 +0100

    extract test from tap

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const tap = require('tap')
+const { test } = require('tap')
 
 const {
   fetchSearchHtml,
@@ -9,38 +9,38 @@ const {
   extractReviewFromHtml
 } = require('.')
 
-tap.test('get page html content', async t => {
+test('get page html content', async t => {
   const html = await fetchSearchHtml('porta carta credito')
   t.plan(1)
   t.true(html.indexOf('s-search-results'))
 })
 
-tap.test('get product details', async t => {
+test('get product details', async t => {
   const html = await fetchProductDetailsHtml('B07JML91PY')
   t.plan(1)
   t.true(html.indexOf('B07JML91PY'))
 })
 
-tap.test('get product reviews', async t => {
+test('get product reviews', async t => {
   const html = await fetchProductReviewsHtml('B07JML91PY')
   t.plan(1)
   t.true(html.indexOf('B07JML91PY'))
 })
 
-tap.test('get product reviews paginated', async t => {
+test('get product reviews paginated', async t => {
   const html = await fetchProductReviewsHtml('B07JML91PY', 2)
   t.plan(1)
   t.true(html.indexOf('B07JML91PY'))
 })
 
-tap.test('parses product reviews paginated', async t => {
+test('parses product reviews paginated', async t => {
   const html = await fetchProductReviewsHtml('B07JML91PY')
   const reviews = parseProductReviews(html)
   t.plan(1)
   t.true(reviews)
 })
 
-tap.test('extracts review from html', async t => {
+test('extracts review from html', async t => {
   const html = await fetchProductReviewsHtml('B07JML91PY')
   const reviews = parseProductReviews(html)
   const review = extractReviewFromHtml(reviews[0])
@@ -51,7 +51,7 @@ tap.test('extracts review from html', async t => {
   t.true(review.text)
 })
 
-tap.test('gets reviews from product search', async t => {
+test('gets reviews from product search', async t => {
   const reviews = await getProductReviews('B07JML91PY')
   const review = reviews[0]
   t.plan(4)
@@ -61,7 +61,7 @@ tap.test('gets reviews from product search', async t => {
   t.true(review.text)
 })
 
-tap.test('gets reviews from product search with proxy', async t => {
+test('gets reviews from product search with proxy', async t => {
   const reviews = await getProductReviews('B07JML91PY', 1, { useProxy: true })
   const review = reviews[0]
   t.plan(4)
