0141e9d778988bfa451649923d4afa46d4b5900e
Christian Fei <crifei93@gmail.com>
2019-11-11 21:38:42 +0100

    fix queue

--- bin.js
+++ bin.js
@@ -25,7 +25,8 @@ if (require.main === module) {
   module.exports = main
 }
 
-async function main (asin, pageNumber = 1) {
+async function main (asin, startingPageNumber = 1) {
+  log({ startingPageNumber })
   const httpInstance = server()
 
   const stats = {
@@ -45,17 +46,17 @@ async function main (asin, pageNumber = 1) {
   }
 
   queue.on('active', function () {
-    log(`Working on ${stats.pageCount++}. Size: ${queue.size}  Pending: ${queue.pending}`)
+    log(`Working on ${stats.pageCount}. Size: ${queue.size}  Pending: ${queue.pending}`)
   })
 
-  const productReviewsCount = await amazon.getProductReviewsCount({ asin, pageNumber }, { puppeteer: true })
+  const productReviewsCount = await amazon.getProductReviewsCount({ asin, pageNumber: startingPageNumber }, { puppeteer: true })
   if (!Number.isFinite(productReviewsCount)) {
     log(`invalid reviews count ${productReviewsCount}`)
     return []
   }
   stats.productReviewsCount = productReviewsCount
 
-  const { reviews: firstPageReviews } = await amazon.scrapeProductReviews({ asin, pageNumber }, { puppeteer: true })
+  const { reviews: firstPageReviews } = await amazon.scrapeProductReviews({ asin, pageNumber: startingPageNumber }, { puppeteer: true })
 
   stats.pageSize = firstPageReviews.length
   stats.pages = parseInt(productReviewsCount / stats.pageSize, 10) + 1
@@ -72,6 +73,7 @@ async function main (asin, pageNumber = 1) {
     log(`Processing ${job.data.pageNumber} / ${stats.pages} (lastPageSize ${stats.lastPageSize})`)
     try {
       await scrape({ asin, pageNumber: job.data.pageNumber }).then(processProductReviews({ asin, pageNumber: job.data.pageNumber }))
+      Object.assign(stats, { pageCount: job.data.pageNumber })
     } catch (err) {
       log(`failed job ${err.message}`, err)
     }
@@ -81,9 +83,10 @@ async function main (asin, pageNumber = 1) {
     done()
   })
 
-  const pageNumbers = Array.from({ length: stats.pages - pageNumber + 1 }, (_, i) => i + pageNumber)
+  const pageNumbers = Array.from({ length: stats.pages - startingPageNumber + 1 }, (_, i) => i + startingPageNumber)
 
   for (const pageNumber of pageNumbers) {
+    log('adding', { pageNumber })
     await pages.add({ pageNumber })
   }
 
@@ -98,14 +101,14 @@ async function main (asin, pageNumber = 1) {
     console.log({ htmlPath, asinPageNumberExistsHTML, jsonPath, asinPageNumberExistsJSON })
 
     let task
-    if (asinPageNumberExistsJSON && !process.env.NO_CACHE) {
+    if (asinPageNumberExistsJSON) {
       task = queue.add(() => {
-        log(`Using html/${asin}-${pageNumber}.html`)
+        log(`Using json/${asin}-${pageNumber}.json`)
         const content = fs.readFileSync(jsonPath, { encoding: 'utf8' })
         const reviews = JSON.parse(content)
         return { reviews }
       })
-    } else if (asinPageNumberExistsHTML && !process.env.NO_CACHE) {
+    } else if (asinPageNumberExistsHTML) {
       task = queue.add(async () => {
         log(`Using html/${asin}-${pageNumber}.html`)
         const html = fs.readFileSync(htmlPath, { encoding: 'utf8' })
