ff300c70e3b962e4c970b1d51b17e70610562926
Christian Fei <crifei93@gmail.com>
2019-11-16 20:17:17 +0100

    retry

--- scrape.js
+++ scrape.js
@@ -74,40 +74,55 @@ function amazonItScraper (initialUrl) {
         const url = productReviewsUrl({ asin, pageNumber })
 
         log('scraping', url)
-        const page = await b.newPage(url)
-
-        const content = await page.content()
-        log(`saving html/${asin}/${asin}-${pageNumber}.html`)
-        try { fs.mkdirSync(path.resolve(__dirname, 'html', asin)) } catch (err) { }
+        let content = ''
         try {
-          fs.writeFileSync(path.resolve(__dirname, 'html', asin, `${asin}-${pageNumber}.html`), content, { encoding: 'utf8' })
-        } catch (err) { console.error(err.message, err.stack) }
+          const page = await b.newPage(url)
 
-        const reviews = parseProductReviews(content)
-          .map(r => Object.assign(r, { asin, pageNumber, url }))
-
-        log(`parsed ${reviews && reviews.length} reviews`)
-        log(`saving json/${asin}/${asin}-${pageNumber}.json`)
-        try {
-          fs.mkdirSync(path.resolve(__dirname, 'json', asin))
-        } catch (err) { }
-        try {
-          fs.writeFileSync(path.resolve(__dirname, 'json', `${asin}/${asin}-${pageNumber}.json`), JSON.stringify(reviews), { encoding: 'utf8' })
-        } catch (err) { console.error(err.message, err.stack) }
-
-        done()
-        if (reviews.length > 0) {
-          reviews.map(r => eventEmitter.emit('review', r))
-          await queue.add({ asin, pageNumber: pageNumber + 1 })
-        } else {
-          if (/captcha/gi.test(content)) {
-            log('found captcha, requeue-ing', pageNumber)
-            return job.retry()
+          content = await page.content()
+          if (content && /captcha/gi.test(content)) {
+            log('found captcha', pageNumber, asin)
+            throw new Error('captcha')
           }
+
+          log(`saving html/${asin}/${asin}-${pageNumber}.html`)
+          try { fs.mkdirSync(path.resolve(__dirname, 'html', asin)) } catch (err) { }
+          try {
+            fs.writeFileSync(path.resolve(__dirname, 'html', asin, `${asin}-${pageNumber}.html`), content, { encoding: 'utf8' })
+          } catch (err) { console.error(err.message, err.stack) }
+
+          const reviews = parseProductReviews(content)
+            .map(r => Object.assign(r, { asin, pageNumber, url }))
+
+          log(`parsed ${reviews && reviews.length} reviews`)
+          log(`saving json/${asin}/${asin}-${pageNumber}.json`)
+          try {
+            fs.mkdirSync(path.resolve(__dirname, 'json', asin))
+          } catch (err) { }
+          try {
+            fs.writeFileSync(path.resolve(__dirname, 'json', `${asin}/${asin}-${pageNumber}.json`), JSON.stringify(reviews), { encoding: 'utf8' })
+          } catch (err) { console.error(err.message, err.stack) }
+
+          if (reviews.length > 0) {
+            reviews.map(r => eventEmitter.emit('review', r))
+            await queue.add({ asin, pageNumber: pageNumber + 1 })
+          }
+          done()
+        } catch (err) {
+          log(`job failed ${job.id} ${job.data}`, err.message, err)
+          log('taking lock', job.id)
+          await job.takeLock()
+          log('took lock', job.id)
+          log('moving to failed', job.id)
+          await job.moveToFailed(new Error(`captcha on ${pageNumber} page for ${asin}`))
+          log('moved to failed', job.id)
+          log('retrying', job.id)
+          await job.retry()
+          log('retried', job.id)
+          done(new Error(`job failed ${job.id} ${err.message}`))
         }
       })
 
-      queue.add({ asin, pageNumber: 1 })
+      queue.add({ asin, pageNumber: 1 }, { attempts: 3, timeout: 5000 })
 
       queue.on('drained', async function () {
         log('-- queue drained')
