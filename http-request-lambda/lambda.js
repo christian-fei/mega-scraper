89bd193151a01b39cdadc6ced67bbb3f7e1093af
Christian Fei <crifei93@gmail.com>
2019-11-18 19:55:20 +0100

    remove http-request-lambda

--- http-request-lambda/lambda.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const main = require('./index')
-
-exports.handler = function ({ url }, context, cb) {
-  main({ url })
-    .then((data) => cb(null, data))
-    .catch(err => cb(err))
-}
