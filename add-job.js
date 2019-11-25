adff5e83ed888775fc97819819823c3942116a44
Christian Fei <crifei93@gmail.com>
2019-11-25 23:45:18 +0100

    poc backend

---
+++ add-job.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+const debug = require('debug')
+debug.enable('mega-scraper:*')
+const log = debug('mega-scraper:add-job')
+const { createQueue } = require('./lib/queue')
+
+if (require.main === module) {
+  addJob(process.argv[2])
+    .then((job) => {
+      log('added job', job && job.id)
+      process.exit(0)
+    })
+    .catch(() => process.exit(1))
+
+  process.on('unhandledRejection', err => log(err.message, err))
+  process.on('uncaughtException', err => log(err.message, err))
+} else {
+  module.exports = { addJob }
+}
+
+async function addJob (url) {
+  log('add job', url)
+  const queue = createQueue('scraper')
+  return queue.add({ url })
+}
